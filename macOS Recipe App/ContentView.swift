dc572cb81962085b70a0c766e32ba234cd05683c
royataya <roy.ataya@hotmail.ca>
2023-06-04 14:03:34 -0700

    Initial Commit

---
+++ macOS Recipe App/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  macOS Recipe App
+//
+//  Created by Roy Ataya on 2023-06-04.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ macOS Recipe App/macOS_Recipe_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  macOS_Recipe_AppApp.swift
+//  macOS Recipe App
+//
+//  Created by Roy Ataya on 2023-06-04.
+//
+
+import SwiftUI
+
+@main
+struct macOS_Recipe_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
